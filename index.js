da9fa9de5fa1b3b20354c1d1102296605eb23be1
ArberSh <arbershaska2@gmail.com>
2023-11-01 23:01:07 +0100

    Added some codes in js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 // service_i27hp1g
 // template_15dhnol
 // _E7mjLykzcx9CZZ_9
+
+function contact(event){
+    event.preventDefault()
+    // emailjs
+    // .sendForm(
+    //     'service_i27hp1g',
+    //     'template_15dhnol',
+    //     event.target,
+    //     '_E7mjLykzcx9CZZ_9'
+    // ).then(() => {
+    //     console.log('this worked')
+    // })
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    setTimeout(()=>{
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible";
+        console.log('it worked 1')
+    },1000);
+}
