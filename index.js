83dc3ea597f919ed1275826f82c696f0f114f927
ArberSh <arbershaska2@gmail.com>
2023-11-02 23:23:49 +0100

    Finished Modal and Added Dark Light

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-// service_i27hp1g
-// template_15dhnol
-// _E7mjLykzcx9CZZ_9
+ let isModalOpen = false;
+ let contrastToggle = false
+
+ function toggleContrast(){
+    contrastToggle = !contrastToggle
+    if(contrastToggle){
+    document.body.classList += " dark-theme"
+    }
+    else{
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event){
     event.preventDefault()
-    // emailjs
-    // .sendForm(
-    //     'service_i27hp1g',
-    //     'template_15dhnol',
-    //     event.target,
-    //     '_E7mjLykzcx9CZZ_9'
-    // ).then(() => {
-    //     console.log('this worked')
-    // })
     const loading = document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
     loading.classList += " modal__overlay--visible"
-    setTimeout(()=>{
+    emailjs
+    .sendForm(
+        'service_i27hp1g',
+        'template_15dhnol',
+        event.target,
+        '_E7mjLykzcx9CZZ_9'
+    ).then(() => { 
         loading.classList.remove("modal__overlay--visible")
         success.classList += " modal__overlay--visible";
-        console.log('it worked 1')
-    },1000);
+    }).catch(()=>{
+    loading.classList += " modal__overlay--visible"
+    alert(
+        "The email service is temporarily unavailable. Please contact me directly on arbershaska2@gmail.com"
+    )
+})
+}
+
+
+function toggleModal(){
+    if (isModalOpen){
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
+    document.body.classList += " modal--open"
 }
